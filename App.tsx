cc9fb60895089f334d5c5ec1a245c7804e014bf3
PranksterDr <diopavel@yandex.ru>
2023-02-12 00:30:51 +0500

    add bottomBar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './src/screens/HomeScreen'
+import BottomTab from './src/screens/BottomTab'
+
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <BottomTab />
   )
 }
